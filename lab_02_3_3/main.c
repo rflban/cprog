73d96df0a3d6dd4c110ada978679158625055250
fnabiev <alsn_5@mail.ru>
2018-04-14 11:46:31 +0300

    Task 3 was updated.

--- lab_02_3_3/main.c
+++ lab_02_3_3/main.c
@@ -4,14 +4,15 @@
 #define EOF_ERROR_M "Error. End of file was reached too early."
 
 double d_abs(double num);
+double aproximate_atan(double x, double eps);
+
 int double_input(double * number, char start_message[], char error_message[]);
 
 int main(void)
 {
     double x;
     double eps;
-    double s, d, f;
-    double numenator, denominator, koef;
+    double apr_f, f;
     int input_state;
 
     input_state = double_input(&x, "Enter x: ", INPUT_ERROR_M);
@@ -28,26 +29,13 @@ int main(void)
         return 0;
     }
 
-    s = 0;
-    d = 1;
-    numenator = x;
-    denominator = 1.0;
-    koef = 1.0;
-    while(d_abs(d) >= eps)
-    {
-        d = koef * numenator/denominator;
-        s += d;
-        koef *= -1;
-        numenator *= x*x;
-        denominator += 2;
-    }
-
+    apr_f = aproximate_atan(x, eps);
     f = atan(x);
 
-    printf("%.4lf\n", s);
+    printf("%.4lf\n", apr_f);
     printf("%.4lf\n", f);
-    printf("%.4lf\n", d_abs(s-f));
-    printf("%.4lf\n", d_abs((s-f)/f));
+    printf("%.4lf\n", d_abs(apr_f-f));
+    printf("%.4lf\n", d_abs((apr_f-f)/f));
 
     return 0;
 }
@@ -77,4 +65,23 @@ int double_input(double * number, char start_message[], char error_message[])
 double d_abs(double num)
 {
     return num > 0 ? num : -num;
+}
+
+double aproximate_atan(double x, double eps)
+{
+    double s = 0;
+    double d = 1;
+    double numenator = x;
+    double denominator = 1.0;
+    double koef = 1.0;
+    while(d_abs(d) >= eps)
+    {
+        d = koef * numenator/denominator;
+        s += d;
+        koef *= -1;
+        numenator *= x*x;
+        denominator += 2;
+    }
+
+    return s;
 }
