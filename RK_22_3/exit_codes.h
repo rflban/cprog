98e9a96fa6448242222e3eb0a345e4fbd49c5abe
U-VLAB\user-lab01 <user-lab01@LAB-01-70.vlab.bmstu.ru>
2018-05-19 14:14:03 +0300

    Initial files were added.

---
+++ RK_22_3/exit_codes.h
@@ -0,0 +1,6 @@
+#ifndef EXIT_CODES_H
+#define EXIT_CODES_H
+
+#define SUCCESS 0
+
+#endif
---
+++ RK_22_3/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "exit_codes.h"
+
+
+int main(int argc, char** argv)
+{
+    
+    return SUCCESS;
+}
