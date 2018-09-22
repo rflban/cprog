2f8eacf11e34577bfb68aa93ed20888d525b8657
fnabiev <alsn_5@mail.ru>
2018-09-22 10:17:24 +0300

    Soert testing modified.

--- lab_07_1/sort_test.c
+++ lab_07_1/sort_test.c
@@ -5,11 +5,10 @@
 
 #include "array_process.h"
 
-#define N1 10
-#define N2 50
-#define N3 100
-#define N4 500
-#define N5 1000
+#define START 0
+#define FINISH 10000
+#define STEP 10
+#define REPEATS 5
 
 unsigned long long tick(void);
 void array_fill(numb* array, int len);
@@ -17,73 +16,44 @@ void array_fill(numb* array, int len);
 
 int main (void)
 {
-    numb array1[N1];
-    numb array2[N2];
-    numb array3[N3];
-    numb array4[N4];
-    numb array5[N5];
+    FILE *out1, *out2;
+    numb array[FINISH] = {0};
     unsigned long long tb, te;
 
     setbuf(stdout, NULL);
     srand(time(NULL));
 
-    array_fill(array1, N1);
-    array_fill(array2, N2);
-    array_fill(array3, N3);
-    array_fill(array4, N4);
-    array_fill(array5, N5);
+    out1 = fopen("sort_out_1.txt", "w");
+    out2 = fopen("sort_out_2.txt", "w");
+    for (int i = START; i <= FINISH; i += STEP)
+    {
+        unsigned long long tacts;
 
-    tb  = tick();
-    mysort(array1, N1, sizeof(numb), comparator);
-    te = tick();
-    printf("%llu\n", (te - tb));
+        tacts = 0;
+        for (int j = 0; j < REPEATS; j++)
+        {
+            array_fill(array, i);
+            tb = tick();
+            mysort(array, i, sizeof(numb), comparator);
+            te = tick();
+            tacts += te - tb;
+        }
+        fprintf(out1, "%llu\n", (tacts / REPEATS));
 
-    tb  = tick();
-    mysort(array2, N2, sizeof(numb), comparator);
-    te = tick();
-    printf("%llu\n", (te - tb));
+        tacts = 0;
+        for (int j = 0; j < REPEATS; j++)
+        {
+            array_fill(array, i);
+            tb = tick();
+            qsort(array, i, sizeof(numb), comparator);
+            te = tick();
+            tacts += te - tb;
+        }
+        fprintf(out2, "%llu\n", (tacts / REPEATS));
+    }
 
-    tb  = tick();
-    mysort(array3, N3, sizeof(numb), comparator);
-    te = tick();
-    printf("%llu\n", (te - tb));
-
-    tb  = tick();
-    mysort(array4, N4, sizeof(numb), comparator);
-    te = tick();
-    printf("%llu\n", (te - tb));
-
-    tb  = tick();
-    mysort(array5, N5, sizeof(numb), comparator);
-    te = tick();
-    printf("%llu\n", (te - tb));
-
-    printf("\n");
-
-    tb  = tick();
-    qsort(array1, N1, sizeof(numb), comparator);
-    te = tick();
-    printf("%llu\n", (te - tb));
-
-    tb  = tick();
-    qsort(array2, N2, sizeof(numb), comparator);
-    te = tick();
-    printf("%llu\n", (te - tb));
-
-    tb  = tick();
-    qsort(array3, N3, sizeof(numb), comparator);
-    te = tick();
-    printf("%llu\n", (te - tb));
-
-    tb  = tick();
-    qsort(array4, N4, sizeof(numb), comparator);
-    te = tick();
-    printf("%llu\n", (te - tb));
-
-    tb  = tick();
-    qsort(array5, N5, sizeof(numb), comparator);
-    te = tick();
-    printf("%llu\n", (te - tb));
+    fclose(out1);
+    fclose(out2);
 
     return 0;
 }
@@ -92,9 +62,10 @@ int main (void)
 unsigned long long tick(void)
 {
     unsigned long long d;
-    __asm__ __volatile__ ("rdtsc" : "=A" (d) );
+    unsigned long long a;
+    __asm__ __volatile__ ("rdtsc" : "=a"(a), "=d"(d));
 
-    return d;
+    return ((d<<32) | a);
 }
 
 void array_fill(numb* array, int len)
