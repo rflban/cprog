25c071c9d74c592bb9b022fd8b31b349aabd47e8
fnabiev <alsn_5@mail.ru>
2018-09-23 16:30:16 +0300

    test out

--- lab_07_1/array_process.c
+++ lab_07_1/array_process.c
@@ -52,7 +52,8 @@ void print_array(FILE *destination, numb *const pb_dst, numb *const pe_dst)
     for (numb *pc = pb_dst; pc <= pe_dst; pc++)
     {
         fprintf(destination, NUM_FORMAT, *pc);
-        fprintf(destination, (pc != pe_dst ? " " : ""));
+        //fprintf(destination, (pc != pe_dst ? " " : ""));
+        fprintf(destination, "\n");
     }
 }
 
@@ -118,11 +119,12 @@ int key(const numb *pb_src, const numb *pe_src, numb **pb_dst, numb **pe_dst)
     len = (max_pos - min_pos + 1) - 2;
     if (len <= 0)
         return EXIT_EMPTY_ARRAY_;
-    *pb_dst = (numb*)malloc(len * sizeof(numb));
+    
+    *pb_dst = (numb*)calloc(len, sizeof(numb));
     if (*pb_dst == NULL)
         return EXIT_NULL_POINTER_REQ_;
+    
     *pe_dst = *pb_dst + len - 1;
-
     numb *pc_dst = *pb_dst;
     for (const numb *pc_src = min_pos + 1; pc_src < max_pos; pc_src++, pc_dst++)
     {
