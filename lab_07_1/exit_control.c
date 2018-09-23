361feea5aac0c58811d6c906426abfa89cc8dc5d
fnabiev <alsn_5@mail.ru>
2018-09-23 19:05:07 +0300

    exit code test 3

--- lab_07_1/exit_control.c
+++ lab_07_1/exit_control.c
@@ -55,7 +55,7 @@ void exitcode_processing(FILE *error_output, const int exit_code)
         case EXIT_EMPTY_INPUT_:
             fprintf(error_output, "Empty input.\n");
             //exit(EXIT_EMPTY_INPUT_);
-            exit(0);
+            exit(EXIT_NULL_POINTER_REQ_);
             break;
         case EXIT_NULL_POINTER_REQ_:
             fprintf(error_output, "Null pointer request.\n");
