65b8cada1aee2a9b98eca31f8602b28414196d78
fnabiev <alsn_5@mail.ru>
2018-09-26 00:01:52 +0300

    output check

--- lab_07_1/array_process.c
+++ lab_07_1/array_process.c
@@ -52,7 +52,7 @@ void print_array(FILE *destination, numb *const pb_dst, numb *const pe_dst)
     for (numb *pc = pb_dst; pc < pe_dst; pc++)
     {
         fprintf(destination, NUM_FORMAT, *pc);
-        fprintf(destination, (pc != (pe_dst - 1) ? "\n" : ""));
+        fprintf(destination, (pc != (pe_dst - 1) ? " " : ""));
         //fprintf(destination, ("\n"));
     }
 }
