95c218ca302edcc29872e0944d1bfb1fde6a6b12
fnabiev <alsn_5@mail.ru>
2018-09-25 23:57:02 +0300

    Check exit codes checking

--- lab_07_1/exit_control.h
+++ lab_07_1/exit_control.h
@@ -3,13 +3,13 @@
 
 #define EXIT_SUCCESS_ 0
 #define EXIT_USAGE_ERROR_ -2
-#define EXIT_OPEN_ERROR_ -2
-#define EXIT_CLOSE_ERROR_ -2
-#define EXIT_FILE_CR_ERROR_ -2
-#define EXIT_INPUT_ERROR_ -2
-#define EXIT_EMPTY_INPUT_ -2
-#define EXIT_EMPTY_ARRAY_ -2
-#define EXIT_NULL_POINTER_REQ_ -2
+#define EXIT_OPEN_ERROR_ -3
+#define EXIT_CLOSE_ERROR_ -4
+#define EXIT_FILE_CR_ERROR_ -5
+#define EXIT_INPUT_ERROR_ -6
+#define EXIT_EMPTY_INPUT_ -7
+#define EXIT_EMPTY_ARRAY_ -8
+#define EXIT_NULL_POINTER_REQ_ -9
 
 
 void exitcode_processing(FILE *error_output, const int exit_code);
