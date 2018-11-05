55559abe2dc2c651a54ef0c209b1849d17959993
fnabiev <alsn_5@mail.ru>
2018-11-05 17:18:09 +0300

    fix my_getline *lineptr processing

--- lab_09_2/my_str.c
+++ lab_09_2/my_str.c
@@ -19,8 +19,16 @@ ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
     size_t buf_size = BUFFER_INIT_SIZE;
     ssize_t read_counter = 0;
 
-    if (*lineptr == NULL)
-        *n = 0;
+    if (*lineptr == NULL || *n == 0)
+    {
+        *n = BUFFER_INIT_SIZE;
+        *lineptr = (char*)malloc(*n);
+        if (!(*lineptr))
+        {
+            exit_code = __EXIT_MEM_ERROR;
+            return EOF;
+        }
+    }
 
     buffer = (char*)malloc(buf_size);
     if (!buffer)
