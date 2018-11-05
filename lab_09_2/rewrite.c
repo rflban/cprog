42a21ff72023029d4f21375c4106b68bdc82027c
fnabiev <alsn_5@mail.ru>
2018-11-05 12:21:54 +0300

    cut rewriting

--- lab_09_2/rewrite.c
+++ lab_09_2/rewrite.c
@@ -4,6 +4,7 @@ void rewrite(FILE *source, FILE *destination, const char *search, const char *re
 {
     while (!feof(source))
     {
+        return;
         char *str;
         char *rewrited_str;
         size_t str_size = 0;
@@ -14,8 +15,6 @@ void rewrite(FILE *source, FILE *destination, const char *search, const char *re
             exit_code = __EXIT_MEM_ERROR;
             return;
         }
-
-        return;
 
         my_getline(&str, &str_size, source);
         if (exit_code != __EXIT_SUCCESS)
