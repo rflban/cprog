dd7243ad462f83cfb1d54a2d9acf17cc4da9764f
fnabiev <alsn_5@mail.ru>
2018-11-05 12:07:04 +0300

    remove rewriting

--- lab_09_2/main.c
+++ lab_09_2/main.c
@@ -26,14 +26,14 @@ int main(int argc, char **argv)
     exit_process(1);
     outputf = openf(argv[2], "w");
     exit_process(1);
-
+return __EXIT_SUCCESS;
     rewrite(inputf, outputf, argv[4], argv[6]);
     exit_process(1);
 
-    /*closef(inputf);
+    closef(inputf);
     exit_process(1);
     closef(outputf);
-    exit_process(1);*/
+    exit_process(1);
 
     return __EXIT_SUCCESS;
 }
