122815dafa7c5904a5e98d3f74f1df8490c2660d
fnabiev <alsn_5@mail.ru>
2018-12-21 06:08:38 +0300

    fix test list alloc

--- lab_10/my_list.c
+++ lab_10/my_list.c
@@ -118,7 +118,10 @@ void remove_duplicates(node_t **head, int (*comparator)(const void*, const void*
         }
 
         compare_result = comparator(previos_node->data, present_node->data);
-exit(0);
+
+        if (previos_node == NULL || present_node == NULL)
+            exit(0);
+
         if (compare_result >= 0)
         {
             previos_node->next = present_node->next;
